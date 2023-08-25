6e90bf8896e97849dea977bc3fa0f45c534e5bcc
hisho <hisho.web@gmail.com>
2023-08-25 21:02:49 +0900

    ✨ change page

--- src/pages/[slug].page.tsx
+++ src/pages/[slug].page.tsx
@@ -2,9 +2,13 @@ import { grayMatterRead } from '@/lib/gray-matter/gray-matter'
 import { NextPageWithLayout } from '@/pages/_app.page'
 import { Slide } from '@/pages/_component/slide'
 import { GetServerSidePropsContext, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import path from 'path'
+import { useEffect } from 'react'
+import { useCounter } from 'react-use'
+import { match } from 'ts-pattern'
 import { z } from 'zod'
 
 export const getStaticPaths = () => {
@@ -38,6 +42,7 @@ export const getStaticProps = async (context: GetServerSidePropsContext) => {
     return {
       props: {
         mdxSource,
+        sections,
       },
     }
   } catch {
@@ -49,7 +54,33 @@ export const getStaticProps = async (context: GetServerSidePropsContext) => {
 
 const Page: NextPageWithLayout<
   InferGetStaticPropsType<typeof getStaticProps>
-> = ({ mdxSource }) => {
+> = ({ mdxSource, sections }) => {
+  const router = useRouter()
+  const [count, { dec, inc }] = useCounter(
+    Number(router.query['slug']) ?? 1,
+    sections.length,
+    1
+  )
+
+  useEffect(() => {
+    const keyDownEvent = (e: globalThis.KeyboardEvent) => {
+      match(e.key.toLowerCase())
+        .with('ArrowRight'.toLowerCase(), () => inc())
+        .with('ArrowLeft'.toLowerCase(), () => dec())
+        .otherwise(() => {})
+    }
+    window.addEventListener('keydown', keyDownEvent, false)
+  }, [dec, inc])
+
+  useEffect(() => {
+    router.push({
+      pathname: router.pathname,
+      query: {
+        slug: count,
+      },
+    })
+  }, [count, router])
+
   return (
     <Slide>
       <MDXRemote {...mdxSource} />
