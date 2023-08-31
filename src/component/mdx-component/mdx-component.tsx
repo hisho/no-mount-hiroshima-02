4f645be6791a4bde111bd3befe5e093a9a8a9952
hisho <hisho.web@gmail.com>
2023-09-01 00:26:58 +0900

    💄 UI

--- src/component/mdx-component/mdx-component.tsx
+++ src/component/mdx-component/mdx-component.tsx
@@ -1,4 +1,5 @@
 import { SyntaxHighlighter } from '@/component/syntax-highlighter/syntax-highlighter'
+import { cn } from '@/util/cn/cn'
 import { MDXContentProps, getMDXComponent } from 'mdx-bundler/client'
 import { useMemo } from 'react'
 import { P, match } from 'ts-pattern'
@@ -10,6 +11,21 @@ const components = {
         return <SyntaxHighlighter code={children} lang={'tsx'} />
       })
       .otherwise((children) => <code>{children}</code>),
+  h1: ({ className, ...props }) => (
+    <h1 className={cn('text-60 font-bold', className)} {...props} />
+  ),
+  h2: ({ className, ...props }) => (
+    <h2 className={cn('text-50 font-bold', className)} {...props} />
+  ),
+  h3: ({ className, ...props }) => (
+    <h3 className={cn('text-40 font-bold', className)} {...props} />
+  ),
+  h4: ({ className, ...props }) => (
+    <h4 className={cn('text-30 font-bold', className)} {...props} />
+  ),
+  h5: ({ className, ...props }) => (
+    <h4 className={cn('text-20 font-bold', className)} {...props} />
+  ),
 } satisfies MDXContentProps['components']
 
 type Props = {
